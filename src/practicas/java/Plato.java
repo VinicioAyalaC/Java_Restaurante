1cfccd0da6bf347c15dafb9ba8452497fb03564f
VinicioAyalaC <vhac2311@gmail.com>
2026-07-18 12:46:53 -0500

    Crear clase plato

---
+++ src/practicas/java/Plato.java
@@ -0,0 +1,10 @@
+package practicas.java;
+
+public class Plato {
+	
+	public String nombre;
+	public String tipo;
+	public float precio;
+	public boolean disponibilidad;
+
+}
---
+++ src/practicas/java/TestPlato.java
@@ -0,0 +1,12 @@
+package practicas.java;
+
+public class TestPlato {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
